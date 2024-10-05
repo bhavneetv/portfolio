2eacb5d1eed85c1060175a85cdc963ab4fb1125d
bhavneet verma <bhavneetv295@gmail.com>
2024-10-05 16:35:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(function(){
+    $('#menu').slicknav({
+        'label':'',
+        
+        // 'brand':'Bhavdet Verdfma',
+        'brand':'<i class="fa-solid fa-code" id="x"></i> Bhavneet Verma'
+    });
+});
+
+
+
+AOS.init();
